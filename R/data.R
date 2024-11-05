44e5a8aa787261095463f7fd93e653698dcb6d6a
corneileM <corneile.minnaar@gmail.com>
2024-11-05 20:03:19 +0200

    Update documentation for functions and datasets

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Pollinator visits rates to flowers
+#'
+#' A fictional dataseT of five different pollinator species and their proportional visit rates to 10 different flower species.
+#'
+#' @format A data frame with 5 rows and 10 variables:
+#' \describe{
+#'   \item{A}{Proportional visit rate to flower species "A"}
+#'   \item{B}{Proportional visit rate to flower species "B"}
+#'   \item{C}{Proportional visit rate to flower species "C"}
+#'   \item{D}{Proportional visit rate to flower species "D"}
+#'   \item{E}{Proportional visit rate to flower species "E"}
+#'   \item{F}{Proportional visit rate to flower species "F"}
+#'   \item{G}{Proportional visit rate to flower species "G"}
+#'   \item{H}{Proportional visit rate to flower species "H"}
+#'   \item{I}{Proportional visit rate to flower species "I"}
+#'   \item{J}{Proportional visit rate to flower species "J"}
+#' }
+#' @examples
+#' pollinator_visits
+"pollinator_visits"
