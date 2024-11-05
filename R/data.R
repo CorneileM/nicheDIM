e703823ba6be1c8201e74ea815bb3e9a7426f459
corneileM <corneile.minnaar@gmail.com>
2024-11-05 20:18:46 +0200

    Update package documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Pollinator visits rates to flowers
 #'
-#' A fictional dataseT of five different pollinator species and their proportional visit rates to 10 different flower species.
+#' A fictional dataset of five different pollinator species and their proportional visit rates to 10 different flower species.
 #'
 #' @format A data frame with 5 rows and 10 variables:
 #' \describe{
@@ -18,3 +18,36 @@
 #' @examples
 #' pollinator_visits
 "pollinator_visits"
+
+#' Flower size pair-wise distance matrix
+#'
+#' A fictional matrix of the pair-wise differences in flower corolla diameter between 10 species of flowers.
+#'
+#' @format A 10x10 matrix:
+#' \describe{
+#' }
+#' @examples
+#' size_dist
+"size_dist"
+
+#' Flower color pair-wise distance matrix
+#'
+#' A fictional matrix of the pair-wise differences in mean flower color values (primary wavelengths measured in nm) between 10 species of flowers.
+#'
+#' @format A 10x10 matrix:
+#' \describe{
+#' }
+#' @examples
+#' color_dist
+"color_dist"
+
+#' Flower tube length pair-wise distance matrix
+#'
+#' A fictional matrix of the pair-wise differences in flower corolla tube length (mm) between 10 species of flowers.
+#'
+#' @format A 10x10 matrix:
+#' \describe{
+#' }
+#' @examples
+#' tube_length_dist
+"tube_length_dist"
